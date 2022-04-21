b38134f03134a0735aeeb3d11c068c837a6418a8
kazuna-code <introprogram64@gmail.com>
2022-04-21 13:07:15 +0900

    Update 11_distributed.cpp

--- 03_mpi/11_distributed.cpp
+++ 03_mpi/11_distributed.cpp
@@ -3,6 +3,8 @@
 #include <cmath>
 #include <mpi.h>
 
+#include <cstring>
+
 struct Body {
   double x, y, m, fx, fy;
 };
@@ -27,8 +29,19 @@ int main(int argc, char** argv) {
   MPI_Type_contiguous(5, MPI_DOUBLE, &MPI_BODY);
   MPI_Type_commit(&MPI_BODY);
   for(int irank=0; irank<size; irank++) {
+    /*
     MPI_Send(jbody, N/size, MPI_BODY, send_to, 0, MPI_COMM_WORLD);
     MPI_Recv(jbody, N/size, MPI_BODY, recv_from, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    */
+
+    Body jbody_sub[N/size];
+    MPI_Win win;
+    MPI_Win_create(jbody_sub, N/size*sizeof(Body), sizeof(Body), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+    MPI_Win_fence(0, win);
+    MPI_Put(jbody, N/size, MPI_BODY, send_to, 0, N/size, MPI_BODY, win);
+    MPI_Win_fence(0, win);
+    std::memcpy(jbody, jbody_sub, N/size*sizeof(Body));
+
     for(int i=0; i<N/size; i++) {
       for(int j=0; j<N/size; j++) {
         double rx = ibody[i].x - jbody[j].x;
